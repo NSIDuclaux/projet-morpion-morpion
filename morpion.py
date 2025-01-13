495f324018f4a83dfe8bdc758ea1a99886aaa094
Pierre-Lucg <pl.decreuse@gmail.com>
2025-01-13 08:37:51 +0000

    mise en place de la classe Partie

---
+++ morpion.py
@@ -0,0 +1,26 @@
+from tictactoeterminal import*
+
+class Partie:
+
+    """Classe d'objet créant une partie pour le jeu morpion :
+
+    - Une fois créée, la partie peut être lancée à tout moment
+    - La partie se termine lorsqu'un joueur a gagné ou si la grille de la partie est entièrement remplie"""
+
+    def __init__(self, partie, joueurs):
+        self.partie = morpion
+        self.joueurs = (joueur1, joueur2)
+        joueur1 = 'X'
+        joueur2 = 'O'
+
+    def debut_partie(self):
+        ...
+
+    def grille_pleine(self):
+        if self.partie.nb_tours() == 9:
+            return True
+
+    def fin_partie(self):
+        if self.partie.grille_pleine() == True:
+            self.partie.destroy()
+        ...
