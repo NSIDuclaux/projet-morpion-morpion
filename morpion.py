9e0d2cb61b38ba3fb9d1dbd0082aa586240a6a3f
Pierre-Lucg <pl.decreuse@gmail.com>
2025-01-14 09:41:57 +0000

    méthode lancer-partie + initialisation du compteur

--- morpion.py
+++ morpion.py
@@ -7,21 +7,23 @@ class Partie:
     - Une fois créée, la partie peut être lancée à tout moment
     - La partie se termine lorsqu'un joueur a gagné ou si la grille de la partie est entièrement remplie"""
 
-    def __init__(self, partie, joueurs):
+    def __init__(self, partie, joueurs, compteur):
         self.partie = morpion
         self.joueurs = (joueur1, joueur2)
         joueur1 = 'X'
         joueur2 = 'O'
+        self.compteur = 0
 
-    def debut_partie(self):
-        ...
+    def lancer_partie(self):
+        while not morpion.fin_partie():
+            lancer_tour()
 
     def grille_pleine(self):
-        if self.partie.nb_tours == 9:
+        if morpion.nb_tours == 9:
             return True
 
     def fin_partie(self):
-        if self.partie.grille_pleine() == True:
-            self.partie.destroy()
-        ...
-AAAAAAAAAAAAAAAAAAA
+        if morpion.grille_pleine() == True:
+            morpion.destroy()
+            return "Égalité"
+        
